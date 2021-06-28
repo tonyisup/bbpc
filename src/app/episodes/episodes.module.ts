441ea8aaeaf42061932b1cb78198fdd89209712c
Tonyisup <antonioaccess@gmail.com>
2021-06-27 23:27:31 -0700

    episode 558 + folder change

--- src/app/episodes/episodes.module.ts
+++ src/app/episodes/episodes.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { EpisodesRoutingModule } from './episodes-routing.module';
 import { EpisodesComponent } from './episodes.component';
-import { EpisodeComponent } from './episodes/episode/episode.component';
-import { EpisodeAddComponent } from './episodes/episode-add/episode-add.component';
-import { EpisodeListComponent } from './episodes/episode-list/episode-list.component';
+import { EpisodeComponent } from './components/episode/episode.component';
+import { EpisodeAddComponent } from './components/episode-add/episode-add.component';
+import { EpisodeListComponent } from './components/episode-list/episode-list.component';
 
 
 @NgModule({
