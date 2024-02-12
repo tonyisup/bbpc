ef0277d8ccad2aec0ea9832f0094d95ee3922e8e
Tonyisup <antonioaccess@gmail.com>
2024-02-12 15:17:17 -0800

    logged in email if no name

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -6,7 +6,7 @@ export const Auth: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {sessionData && <span>Logged in as {sessionData.user?.name || sessionData.user?.email}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
