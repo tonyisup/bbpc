01598feba2e8cfcface32f73a63bfa462234b04b
tony mac <antonioaccess@gmail.com>
2026-07-24 14:21:11 -0700

    Migrate year archive and rankings to Convex

--- src/app/year/page.tsx
+++ src/app/year/page.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { YearPageClient } from "./YearPageClient";
+import { env } from "@/env.mjs";
 
 export default async function YearPage() {
+  if (env.NEXT_PUBLIC_BBPC_BACKEND === "convex") {
+    const { ConvexYearPageClient } = await import("./ConvexYearPageClient");
+    return <ConvexYearPageClient />;
+  }
+
+  const { YearPageClient } = await import("./YearPageClient");
   return <YearPageClient />;
 }
